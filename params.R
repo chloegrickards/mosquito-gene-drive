779ba5c6c30bbb0f77a9a26f39b2df6d3c0b99d7
chloegrickards <chloegr@stanford.edu>
2018-03-11 19:51:54 -0700

    Update params.R

--- params.R
+++ params.R
@@ -1,4 +1,5 @@
-#Initial Genotype Conditions
+#Initial Genotype Population
+#Units = number of mosquitoes
 AA <- 99
 AB <- 0
 BB <- 0
@@ -6,8 +7,8 @@ AG <- 0
 BG <- 0
 GG <- 1
 
+#Transition Matrix set-up
 #This contains all the transition probabilities. We'll fill these into our transition matrix.
-
 #Format = currentgenotype_nextgenotype
 
 #Row 1
@@ -28,7 +29,7 @@ AB_GG <- (1/4)*(1-b)*(g)*(AG + BG) + (1/2)*(1-b)*(g)*GG
 
 #Row 3
 BB_AA <- 0
-BB_AB <- AA + (1/2)*(AB + AG) + (1/2)*b*AG
+BB_AB <- AA + (1/2)*(AB + AG)
 BB_BB <- BB + (1/2)*(AB + BG) + (1/2)*b*(AG + BG) + b*GG
 BB_AG <- 0
 BB_BG <- (1/2)*(1-b)*(AG + BG) + (1-b)*GG
@@ -37,18 +38,18 @@ BB_GG <- 0
 #Row 4
 AG_AA <- (1/2)*AA + (1/4)*(AB + AG)
 AG_AB <- (1/2)*b*(AA + AG + GG) + (1/4)*(AB + BG) + (1/4)*b*(AB + BG) + (1/2)*BB
-AG_BB <- (1/2)*b*(BG) + (1/4)*(b^2)*(AG + BG) + (1/4)*b*(AB + BG) + (1/2)*(b^2)*GG
+AG_BB <- (1/2)*b*(BB) + (1/4)*(b^2)*(AG + BG) + (1/4)*b*(AB + BG) + (1/2)*(b^2)*GG
 AG_AG <- (1/2)*(1-b)*(1-g)*(AA + AG + GG) + (1/4)*(1-b)*(1-g)*(AB + BG)
-AG_BG <- (1/4)*(1-b)*(AB + BG) + (1/2)*(1-b)*BB + (1/4)*(2*b*(1-b))*(AG + BG) + (1/2)*(2*b*(1-b))*(GG)
-AG_GG <- (1/2)*(1-b)*g*(AA + AG + GG) + (1/4)*(1-b)*g*(AB + BG) + (1/4)*((1-b)^2)*(AG + BG) + (1/2)*((1-b)^2)*(GG)
+AG_BG <- (1/4)*(1-b)*(AB + BG) + (1/2)*(1-b)*BB + (1/4)*(2*b*(1-b))*(AG + BG) + (1/2)*(2*b*(1-b))*GG
+AG_GG <- (1/2)*(1-b)*g*(AA + AG + GG) + (1/4)*(1-b)*g*(AB + BG) + (1/4)*((1-b)^2)*(AG + BG) + (1/2)*((1-b)^2)*GG
 
 #Row 5
 BG_AA <- 0
 BG_AB <- (1/2)*AA + (1/2)*b*AA + (1/4)*(AB + AG) + (1/4)*b*(AB + AG)
-BG_BB <- (1/4)*(AB + BG) + (1/4)*b*(AB + AG + BG) + (1/2)*BB + (1/2)*b*(BB + GG) + (1/4)*(b^2)*(AG + BG) + (1/2)*(b^2)*(GG)
+BG_BB <- (1/4)*(AB + BG) + (1/4)*b*(AB + AG) + (1/2)*BB + (1/2)*b*(BB + BG + GG) + (1/4)*(b^2)*(AG + BG) + (1/2)*(b^2)*GG
 BG_AG <- (1/2)*(1-b)*(1-g)*AA + (1/4)*(1-b)*(1-g)*(AB + AG)
-BG_BG <- (1/4)*(1-b)*(AB + AG) + (1/2)*(1-b)*(BB + BG + GG) + (1/4)*(2*b*(1-b))*(AG + BG) + (1/2)*(2*b*(1-b))*(GG)
-BG_GG <- (1/2)*(1-b)*(g)*AA + (1/4)*(1-b)*(g)*(AB + AG) + (1/4)*((1-b)^2)*(AG + BG) + (1/2)*((1-b)^2)*(GG)
+BG_BG <- (1/4)*(1-b)*(AB + AG) + (1/2)*(1-b)*(BB + BG + GG) + (1/4)*(2*b*(1-b))*(AG + BG) + (1/2)*(2*b*(1-b))*GG
+BG_GG <- (1/2)*(1-b)*(g)*AA + (1/4)*(1-b)*(g)*(AB + AG) + (1/4)*((1-b)^2)*(AG + BG) + (1/2)*((1-b)^2)*GG
 
 #Row 6                        
 GG_AA <- 0
@@ -56,7 +57,7 @@ GG_AB <- b*AA + (1/2)*b*(AB + AG)
 GG_BB <- b*BB + (1/2)*b*(AB + BG) + (1/2)*(b^2)*(AG + BG) + (b^2)*GG
 GG_AG <- (1-b)*(1-g)*AA + (1/2)*(1-b)*(1-g)*(AB + AG)
 GG_BG <- (1-b)*BB + (1/2)*(1-b)*(AB + BG) + (1/2)*(2*b*(1-b))*(AG + BG) + (2*b*(1-b))*GG
-GG_GG <- (1-b)*g*AA + (1/2)*(1-b)*g*(AB + AG) + (1/2)*((1-b)^2)*(AG + BG) + ((1-b)^2)*(GG)
+GG_GG <- (1-b)*g*AA + (1/2)*(1-b)*g*(AB + AG) + (1/2)*((1-b)^2)*(AG + BG) + ((1-b)^2)*GG
 
 #Concatenate rows
 rowAA <- c(AA_AA, AA_AB, AA_BB, AA_AG, AA_BG, AA_GG)
