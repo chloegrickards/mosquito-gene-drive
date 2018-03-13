84412f6a41f756b76be511b7c5e9e24451e733ae
chloegrickards <chloegr@stanford.edu>
2018-03-13 14:30:48 -0700

    Update psuedocode.R

--- psuedocode.R
+++ psuedocode.R
@@ -11,14 +11,33 @@ for (i in 1:ngen) {
   # genotype(i) = genotypes(i-1)*[transition matrix]
   
   #Gene Drive SEIR
-  #Sg = genotypes of AA, AB -- susceptible to gene drive infection
-  #Eg = genotypes of AG -- carry a gene drive infection
-  #Ig = genotypes of GG -- completely gene drive infected
-  #Rg = genotypes of BB, BG -- cannot
+  #Sg(i) = genotypes of AA, AB -- susceptible to gene drive infection
+  #Eg(i) = genotypes of AG -- carry a gene drive infection
+  #Ig(i) = genotypes of GG -- completely gene drive infected
+  #Rg(i) = genotypes of BB, BG -- cannot
   # possibility of moving BG --> Eg
   
   #Malaria SIR model
-  #Sm = Susceptble mosquites
+  #birth rate of susceptible mosquitoes = AA(i) + AB(i) + BB(i) + (some %)*AG + (some %)*BG(i) + (some %)*GG(i)
+  # = r in the logistic growth equations
+  
+  #Option 2:
+  # modify dXdt
+  
+  #dXdt from Jamie's equations
+  #NOT including mu for birth rate because we already did that above with the genotypes transition matrix
+  #dXdt = (logistic growth function, with birth rates = r) - beta*x*y - death rate
+
+  #X(i) = integrate(logistic growth function, with birth rates = r) + integrate(-beta*x*y - death rate)
+  
+  
+  
+  #X(i) = integrate(dXdt)
+  
+  
+  
+  
+  #Sm(i) = Susceptble mosquites
   # some% = susceptibility fr being heterozygous
   # depends on these susceptible phenotypes: AA, AB, BB, some% AG, some% BG
   # + birth
